1869046e0e6ba3afd8718a87e06a210f586b8c4d
aitestai <143775488+aitestai@users.noreply.github.com>
2023-09-08 20:49:31 +0900

    Export xc

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const { DcinsideApi } = require('./api/api.js');
 const { delay } = require('./utils/delay.js');
-const { jx } = require('./utils/decrypt.js');
+const { jx, xc } = require('./utils/decrypt.js');
 const constants = require('./api/constants.js');
 
 exports.DcinsideApi = DcinsideApi;
 exports.delay = delay;
 exports.jx = jx;
 exports.constants = constants;
+exports.xc = xc;
